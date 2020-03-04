5006c060b3a4b2a3076c20792ee112740d40661e
M.Yasini <yasinimahdi98@gmail.com>
2020-03-04 17:30:15 +0330

    Setup base redux

---
+++ src/store/reducer/countrySetReducer.js
@@ -0,0 +1,11 @@
+import * as actionTypes from '../actions';
+
+const initialState = {
+    countryInfo: {}
+};
+
+const countryReducer = (state = initialState, action) => {
+    return state;
+}
+
+export default countryReducer;

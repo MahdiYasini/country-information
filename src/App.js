c3c181887b4b39105434d0a693d57b654b4603ce
M.Yasini <yasinimahdi98@gmail.com>
2020-03-04 18:04:16 +0330

    define main blog page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Blog from './components/blog/Blog';
 import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Blog/>
       </div>
     </BrowserRouter>
   );
---
+++ src/components/blog/Blog.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+
+const Blog = () => {
+
+};
+
+export default Blog;

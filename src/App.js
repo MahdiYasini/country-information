e365b68aa0cdf4519588b2c9f11db85e64aa7948
M.Yasini <yasinimahdi98@gmail.com>
2020-03-06 19:43:47 +0330

    correce flex box in main page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+import {Box} from '@material-ui/core';
 import Blog from './components/blog/Blog';
 import ChooseCountry from './components/chooseCountry/chooseCountry';
 import CountryFlag from './components/countryFlag/countryFlag';
+import CountryInfo from './components/countryInfo/countryInfo';
 function App() {
   return (
     <BrowserRouter>
           <Blog/>
           <ChooseCountry/>
-          <div style = {{display: 'flex', flexWrap: 'wrap'}}>
-            <CountryFlag/>
-          </div>
+          <Box display="flex" flexDirection="row" flexWrap="wrap">
+            <CountryFlag />
+            <CountryInfo/>
+          </Box>
     </BrowserRouter>
   );
 }

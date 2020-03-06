bf1a15076a8049959e37bb45833c81f0dc64ae96
M.Yasini <yasinimahdi98@gmail.com>
2020-03-06 09:33:01 +0330

    NI

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Blog from './components/blog/Blog';
-import ChooseCountry from './components/chooseCountry/chooseCountry'
+import ChooseCountry from './components/chooseCountry/chooseCountry';
+import CountryFlag from './components/countryFlag/countryFlag';
 function App() {
   return (
     <BrowserRouter>
           <Blog/>
           <ChooseCountry/>
+          <div style = {{display: 'flex', flexWrap: 'wrap'}}>
+            <CountryFlag/>
+          </div>
     </BrowserRouter>
   );
 }
---
+++ src/components/countryFlag/countryFlag.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import * as actionTypes from '../../store/actions';
+import { connect } from 'react-redux';
+import { Box, makeStyles, Select, Typography, Avatar } from '@material-ui/core';
+import amber from '@material-ui/core/colors/amber';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import blueGrey from '@material-ui/core/colors/blueGrey';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > *': {
+            left: 0,
+            margin: theme.spacing(0),
+            width: theme.spacing(16),
+            height: theme.spacing(16),
+        },
+    },
+    box: {
+        display: "flex",
+        // padding: "auto auto",
+        width: "50%",
+        // height: "50%",
+        background: blueGrey[500]
+    },
+    avatar: {
+        width: "50%",
+        margin: "0 auto"
+    }
+
+
+}));
+
+
+const ChooseCountry = (props) => {
+    const classes = useStyles()
+
+
+
+    return (
+        < >
+            <Box className={classes.box} >
+                <img className = {classes.avatar} alt="Remy Sharp" src={props.selectedCountry === -1 ? "https://www.countryflags.io/ir/flat/64.png" : `https://www.countryflags.io/${props.countryListAllIsoData[props.selectedCountry].code}/flat/64.png`} />
+            </Box>
+        </>
+    );
+}
+
+
+const mapStateToProps = state => {
+    if(state.country.countrySelected !== -1){
+        console.log(state.country.countryListAllIsoData[state.country.countrySelected].code)
+    }
+
+    return {
+        selectedCountry: state.country.countrySelected,
+        countryListAllIsoData: state.country.countryListAllIsoData
+    }
+}
+
+export default connect(mapStateToProps)(ChooseCountry);

af9525f37e074b136556b2eb704c1d7d65a447d1
M.Yasini <yasinimahdi98@gmail.com>
2020-03-09 09:18:45 +0330

    NI

--- src/components/countryInfo/countryInfo.js
+++ src/components/countryInfo/countryInfo.js
@@ -5,12 +5,6 @@ import grey from '@material-ui/core/colors/grey';
 import axios from '../../axios';
 import LoaderIcon from '../../UI/loader';
 import * as actionTypes from '../../store/actions'
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 
 const useStyles = makeStyles(theme => ({
     box: {

96bd6b426aa2bc102869c549cd45e46860b079bd
M.Yasini <yasinimahdi98@gmail.com>
2020-03-09 13:02:10 +0330

    404 page set

---
+++ src/UI/404Page/404Page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './404Page.css';
+const errorPage = () => (
+        <div className="container">
+            <p className = "heading">404</p>
+            <p>Capital City not found.</p>
+        </div>
+
+);
+export default errorPage;

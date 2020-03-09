5c030c25acecb00d675908547bc8696655b7f5b4
M.Yasini <yasinimahdi98@gmail.com>
2020-03-09 09:14:57 +0330

    country info final version

--- src/components/countryInfo/countryInfo.js
+++ src/components/countryInfo/countryInfo.js
@@ -4,6 +4,7 @@ import { Paper, Box, makeStyles } from '@material-ui/core';
 import grey from '@material-ui/core/colors/grey';
 import axios from '../../axios';
 import LoaderIcon from '../../UI/loader';
+import * as actionTypes from '../../store/actions'
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -22,41 +23,23 @@ const useStyles = makeStyles(theme => ({
             width: "50%",
         },
     },
-    info: {
-        margin: "0 auto",
-        // width: 180,
-        // height: "90px",
-        // [theme.breakpoints.up('md')]: {
-        //     width: 270,
-        //     // height: "160px",
-        // },
-        // [theme.breakpoints.up('lg')]: {
-        //     width: 650,
-        //     // height: "200px",
-        // },
-        width: 460,
-        height: 319,
-        [theme.breakpoints.up('md')]: {
-            width: 500,
-            height: "160px",
-        },
-        [theme.breakpoints.up('lg')]: {
-            width: 650,
-            height: "200px",
-        },
-    }
-
 }));
 
 const ChooseCountry = (props) => {
-
-
-    const neededItemsForInformation = ['name', 'capital', 'region', 'population', 'languages', 'nativeName'];
+    const neededItemsForInformation = ['name', 'capital', 'region', 'population', 'languages', 'nativeName', 'currencies'];
     const [country, setCountry] = useState('');
     const [countryInfo, setCountryInfo] = useState({});
     const [loader, setLoader] = useState(false);
 
     const classes = useStyles()
+
+    const HandleNumberWithCommas = (x) => {
+        x = x.toString();
+        var pattern = /(-?\d+)(\d{3})/;
+        while (pattern.test(x))
+            x = x.replace(pattern, "$1,$2");
+        return x;
+    }
 
     useEffect(() => {
         if (props.selectedCountry !== -1 && country !== props.countryListAllIsoData[props.selectedCountry].code3.toLowerCase()) {
@@ -64,27 +47,39 @@ const ChooseCountry = (props) => {
             setCountry(props.countryListAllIsoData[props.selectedCountry].code3.toLowerCase());
             axios.get('/' + props.countryListAllIsoData[props.selectedCountry].code3.toLowerCase())
                 .then(response => {
-
                     setCountryInfo(Object.keys(response.data).filter(keys => neededItemsForInformation.includes(keys)).reduce((obj, key) => {
-                        key === "languages" ? obj[key] = response.data[key][0].name : obj[key] = response.data[key];
-                        return obj;
+                        switch (key) {
+                            case "currencies":
+                                obj[key] = response.data[key][0].name
+                                obj['cash_Code'] = response.data[key][0].code
+                                obj['cash_Symbol'] = JSON.stringify(response.data[key][0].symbol)
+                                return obj;
+
+                            case "languages":
+                                obj[key] = response.data[key][0].name
+                                return obj;
+
+                            case "nativeName":
+                                obj[key] = JSON.stringify(response.data[key]);
+                                return obj;
+
+                            case "population":
+                                obj[key] = HandleNumberWithCommas(response.data[key]);
+                                return obj;
+                            default:
+                                obj[key] = response.data[key];
+                                return obj;
+                        }
                     }, {}));
+                    props.onSelectCountryInfo(response.data);
                     setLoader(false);
-                    console.log(response.data);
                 });
         }
-
     });
-    console.log('countryInfo: ', countryInfo);
-    console.log('loader: ', loader);
-
-    (loader === true && Object.keys(countryInfo).length === 0) ? console.log('load') : console.log('ali');
-    //   var size = Object.keys(myObject).length;
-    //! try to make loader in correct time
 
     const countryInformation = (
         <>
-            <div style={{ width: '100%' }}>
+            <div style={{ width: '100%', padding: "2%" }}>
                 <Box
                     justifyContent="column"
                     display="flex"
@@ -93,7 +88,6 @@ const ChooseCountry = (props) => {
                     m={1}
                     bgcolor="background.paper"
                     style={{ maxWidth: 600, margin: "0 auto" }}
-
                 >
                     {Object.keys(countryInfo).map(information => <Box key={information} style={{ fontSize: "18px", color: "#263238" }} p={1} m={1} component={Paper} bgcolor={grey[300]} align="center">{information.toUpperCase()}: {countryInfo[information]}</Box>)}
                 </Box>
@@ -103,25 +97,22 @@ const ChooseCountry = (props) => {
 
     return (
         <div className={classes.box} >
-            {/* {countryInformation} */}
-
-
-            {(loader === true) ? <LoaderIcon /> : countryInformation }
-
-            {/* <img className={classes.avatar} alt="Remy Sharp" src={`https://restcountries.eu/data/irn.svg`} /> */}
-
+            {(loader === true) ? <LoaderIcon /> : countryInformation}
         </div>
     );
 }
 
+const mapDispatchToProps = dispatch => {
+    return {
+        onSelectCountryInfo: (information) => dispatch({ type: actionTypes.COUNTRY_INFO, information: information })
+    }
+}
 
 const mapStateToProps = state => {
     return {
         selectedCountry: state.country.countrySelected,
-        countryListAllIsoData: state.country.countryListAllIsoData
+        countryListAllIsoData: state.country.countryListAllIsoData,
     }
 }
 
-
-
-export default connect(mapStateToProps)(ChooseCountry);
+export default connect(mapStateToProps, mapDispatchToProps)(ChooseCountry);

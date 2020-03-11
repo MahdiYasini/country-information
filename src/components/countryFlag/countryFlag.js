d7c2cd26052073b1bd2481f412e097627024ed2c
M.Yasini <yasinimahdi98@gmail.com>
2020-03-11 12:04:21 +0330

    optimizing

--- src/components/countryFlag/countryFlag.js
+++ src/components/countryFlag/countryFlag.js
@@ -5,28 +5,18 @@ import blueGrey from '@material-ui/core/colors/blueGrey';
 import defaultImage from '../../assets/images/default.jpg'
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        '& > *': {
-            left: 0,
-            margin: theme.spacing(0),
-            width: theme.spacing(16),
-            height: theme.spacing(16),
-        },
-    },
     box: {
         display: "flex",
         width: "100%",
         backgroundColor: blueGrey[900],
-        padding: "2%" ,
+        padding: "2%",
         [theme.breakpoints.up('sm')]: {
             width: "50%",
         },
     },
     avatar: {
-  
-        backgroundSize: "cover", 
+
+        backgroundSize: "cover",
         margin: "0 auto",
         width: "180px",
         height: "90px",
@@ -45,16 +35,15 @@ const useStyles = makeStyles(theme => ({
 const ChooseCountry = (props) => {
     const classes = useStyles();
     return (
-        < >
+        <>
             <Box className={classes.box} >
-                {props.countrySelected === -1 ? <Avatar variant = "rounded" className={classes.avatar} alt="Country"  src= {defaultImage} />:
-                <Avatar variant = "rounded" className={classes.avatar} alt={props.countryListAllIsoData[props.countrySelected].name} src={`https://restcountries.eu/data/${props.countryListAllIsoData[props.countrySelected].code3.toLowerCase()}.svg`} />
+                {props.countrySelected === -1 ? <Avatar variant="rounded" className={classes.avatar} alt="Country" src={defaultImage} /> :
+                    <Avatar variant="rounded" className={classes.avatar} alt={props.countryListAllIsoData[props.countrySelected].name} src={`https://restcountries.eu/data/${props.countryListAllIsoData[props.countrySelected].code3.toLowerCase()}.svg`} />
                 }
             </Box>
         </>
     );
 }
-
 
 const mapStateToProps = state => {
     return {

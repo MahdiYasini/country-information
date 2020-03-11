259c79f0009badf2dd4a6384e3775b2ebfcfe2ed
M.Yasini <yasinimahdi98@gmail.com>
2020-03-11 12:37:56 +0330

    optimizing

--- src/index.js
+++ src/index.js
@@ -11,18 +11,15 @@ import authenticateReducer from './store/reducer/authentication';
 
 import axios from 'axios';
 axios.interceptors.request.use(request => {
-    console.log(request);
     return request;
 }, error => {
-    console.log(error);
     return Promise.reject(error);
 });
 
 axios.interceptors.response.use(response => {
-    console.log(response);
+    response.setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
     return response;
 }, error => {
-    console.log(error);
     return Promise.reject(error);
 });
 

454eef0d095a2885c406f18533aa2d0cf647c177
M.Yasini <yasinimahdi98@gmail.com>
2020-03-11 12:13:58 +0330

    optimizing

--- src/components/mainBlog.js/mainBlog.js
+++ src/components/mainBlog.js/mainBlog.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Box} from '@material-ui/core';
+import { Box } from '@material-ui/core';
 import ChooseCountry from '../chooseCountry/chooseCountry';
 import CountryFlag from '../countryFlag/countryFlag';
 import CountryInfo from '../countryInfo/countryInfo';
@@ -7,20 +7,20 @@ import WeatherOfCapitalCity from '../weatherOfCapitalCity/weatherOfCapitalCity'
 import TelephoneCodeNumber from '../telephoneCodeNumber/telephoneCodeNumber'
 import CountryMap from '../countryMap/countryMap';
 
-const MainBlog = () => (    
-<>
-    <ChooseCountry/>
+const MainBlog = () => (
+  <>
+    <ChooseCountry />
     <Box display="flex" flexWrap="wrap">
       <CountryFlag />
-      <CountryInfo/>
+      <CountryInfo />
     </Box>
     <Box display="flex" flexWrap="wrap">
       <WeatherOfCapitalCity />
       <TelephoneCodeNumber />
       <CountryMap />
     </Box>
-</>
-)
+  </>
+);
 
 export default MainBlog;
 

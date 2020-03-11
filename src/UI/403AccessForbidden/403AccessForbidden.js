f4919dcb04abb6697f374c0454d8d7463f8ac149
M.Yasini <yasinimahdi98@gmail.com>
2020-03-11 11:22:44 +0330

    optimizing

--- src/UI/403AccessForbidden/403AccessForbidden.js
+++ src/UI/403AccessForbidden/403AccessForbidden.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './403AccessForbidden.css';
 const errorPage = () => (
-<div className = "mainBody">
-<h1 className="text"><span>SignUp</span></h1>
-</div>
+    <div className="mainBody">
+        <h1 className="text"><span>SignUp</span></h1>
+    </div>
 );
 export default errorPage;

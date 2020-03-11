0c1525fde7063441f801d3b8d99ddb5d07d8a227
M.Yasini <yasinimahdi98@gmail.com>
2020-03-11 10:17:35 +0330

    Access denied page

---
+++ src/UI/403AccessForbidden/403AccessForbidden.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './403AccessForbidden.css';
+const errorPage = () => (
+<div className = "mainBody">
+<h1 className="text"><span>SignUp</span></h1>
+</div>
+);
+export default errorPage;

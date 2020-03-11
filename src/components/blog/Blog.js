2a601872b0aeefa039a1aea2e5a324668774c8c5
M.Yasini <yasinimahdi98@gmail.com>
2020-03-11 12:04:59 +0330

    NI

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';

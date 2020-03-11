ff0f960da3d48a22b6131bc19216a52817ee2275
M.Yasini <yasinimahdi98@gmail.com>
2020-03-11 11:07:05 +0330

    correct errors

--- src/UI/404CapitalCity/404CapitalCity.js
+++ src/UI/404CapitalCity/404CapitalCity.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './404CapitalCity.css';
 const errorPage = () => (
-        <div className="container">
-            <p className = "heading">404</p>
-            <p>Capital City not found.</p>
-        </div>
+    <>
+        <p className = "header">404</p>
+        <p className = "explain">Capital City not found.</p>
+    </>
 
 );
 export default errorPage;
